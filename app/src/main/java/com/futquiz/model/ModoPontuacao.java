18947faabfc53a511f1bc548c5d2ccbe5c68634b
jpcpsgodoy <jpcpsgodoy@gmail.com>
2025-06-15 02:13:11 -0300

    Adiciona ModoPontuacao para controlar pontuação por TD de passe ou total

---
+++ app/src/main/java/com/futquiz/model/ModoPontuacao.java
@@ -0,0 +1,11 @@
+package com.futquiz.model;
+
+/**
+ * Enumeração que define os modos de pontuação disponíveis no jogo
+ *
+ * @author João Pedro
+ */
+public enum ModoPontuacao {
+    TD_PASSE,
+    TD_TOTAL
+}
