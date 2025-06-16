e2f46b60e32127043a7db40315b19ece6a4ad175
Lari <l177154@dac.unicamp.br>
2025-06-16 14:23:34 -0300

    criar classe RodadaNormal

---
+++ app/src/main/java/com/futquiz/model/RodadaNormal.java
@@ -0,0 +1,25 @@
+package com.futquiz.model;
+
+/*
+ * Classe que representa uma rodada normal no jogo FutQuiz
+ * @author Larissa Palhares
+ */
+public class RodadaNormal extends Rodada {
+    
+    /*
+     * Construtor da classe RodadaNormal
+     */
+    public RodadaNormal(int meta, ModoPontuacao modoPontuacao) {
+        super(meta, modoPontuacao);
+        this.exibeEstatisticas = true;
+    }
+
+    /**
+     * Método que inicia a rodada normal, criando os multiplicadores
+     */
+    @Override
+    public void iniciarRodada() {
+        this.multiplicadores = MultiplicadorFactory.criarMultiplicadores(meta);
+    }
+
+}
