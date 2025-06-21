3d26dc12e5270baf07341a3e6d1cce530807d532
Larissa Palhares <l177154@dac.unicamp.br>
2025-06-21 15:23:38 -0300

    Criar arquivos de exceção TipoRodadaInvalidoException e ModoPontuacaoInvalidoException

---
+++ app/src/main/java/com/futquiz/exceptions/ModoPontuacaoInvalidoException.java
@@ -0,0 +1,18 @@
+package com.futquiz.exceptions;
+
+/*
+ * Exceçao lançada quando o modo de pontuação é inválido.
+ * @author Larissa Palhares
+ */
+public class ModoPontuacaoInvalidoException extends Exception {
+
+    /**
+     * Construtor da exceção.
+     *
+     * @param mensagem Mensagem de erro a ser exibida.
+     */
+    public ModoPontuacaoInvalidoException(String mensagem) {
+        super(mensagem);
+    }
+    
+}
