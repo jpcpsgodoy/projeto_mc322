a2845b5057cdccdc8f09e18227a5e754d5544b24
Larissa Palhares <l177154@dac.unicamp.br>
2025-06-21 15:40:20 -0300

    Cria um factory RodadaFactory

---
+++ app/src/main/java/com/futquiz/model/RodadaFactory.java
@@ -0,0 +1,61 @@
+package com.futquiz.model;
+
+import com.futquiz.exceptions.*;
+/**
+ * Fábrica de rodadas que cria instâncias de Rodada
+ * @author Larissa Palhares
+ */
+public class RodadaFactory {
+
+    /**
+     * Cria uma nova instância de Rodada com base nos parâmetros fornecidos
+     * @param meta              Meta de touchdowns a ser alcançada na rodada
+     * @param modoSelecionado  String que representa o modo de pontuação selecionado
+     * @param tipoSelecionado String que representa o tipo de rodada selecionado
+     * @return Instância de Rodada criada com base nos parâmetros fornecidos
+     * @throws ModoPontuacaoInvalidoException se o modo de pontuação selecionado for inválido
+     * @throws TipoRodadaInvalidoException se o tipo de rodada selecionado for inválido
+     */
+    public static Rodada criarRodada(int meta, String modoSelecionado, String tipoSelecionado)
+                                    throws ModoPontuacaoInvalidoException, TipoRodadaInvalidoException {
+        ModoPontuacao modo = validarModoPontuacao(modoSelecionado);
+        boolean exibeEstatisticas = validarTipoRodada(tipoSelecionado);
+
+        return exibeEstatisticas ? new RodadaNormal(meta, modo)
+                                 : new RodadaDesafio(meta, modo);
+    }
+
+    /**
+     * Valida o modo de pontuação selecionado e retorna o modo correspondente
+     * @param modoSelecionado String que representa o modo de pontuação selecionado
+     * @return ModoPontuacao correspondente ao modo de pontuação selecionado
+     * @throws ModoPontuacaoInvalidoException se o modo de pontuação selecionado for inválido
+     */
+    private static ModoPontuacao validarModoPontuacao(String modoSelecionado) 
+                                                     throws ModoPontuacaoInvalidoException {
+        if (modoSelecionado.equals("Passados")) {
+            return ModoPontuacao.TD_PASSE;
+        } else if (modoSelecionado.equals("Totais")) {
+            return ModoPontuacao.TD_TOTAL;
+        } else {
+            throw new ModoPontuacaoInvalidoException("Escolha um modo de pontuação válido: Passados ou Totais.");
+        }
+    }
+
+    /**
+     * Valida o tipo de rodada selecionado e retorna o boolean correspondente
+     * @param tipoRodada String que representa o tipo de rodada selecionado
+     * @return boolean correspondente ao tipo de rodada selecionado
+     * @throws TipoRodadaInvalidoException se o tipo de rodada selecionado for inválido
+     */
+    private static boolean validarTipoRodada(String tipoRodada) 
+                                            throws TipoRodadaInvalidoException {
+        if (tipoRodada.equals("Normal")) {
+            return true;
+        } else if (tipoRodada.equals("Desafio")) {
+            return false;
+        } else {
+            throw new TipoRodadaInvalidoException("Escolha um tipo de rodada válido: Normal ou Desafio.");
+        }
+    }
+}
